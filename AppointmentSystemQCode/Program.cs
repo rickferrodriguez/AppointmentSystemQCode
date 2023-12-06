55112242798193f7684ca63f6ed5453c6bb628ff
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-12-06 12:06:25 -0500

    new way to set things

--- AppointmentSystemQCode/Program.cs
+++ AppointmentSystemQCode/Program.cs
@@ -1,82 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 
 public class Appointment
 {
-    [JsonProperty("Day")]
-    public string DayOfWeek { get; set; }
+  [JsonProperty("Day")] public string DayOfWeek { get; set; }
 
-    [JsonProperty("Hour")]
-    public TimeSpan StartTime { get; set; }
+  [JsonProperty("Hour")] public TimeSpan StartTime { get; set; }
 
-    [JsonProperty("Duration")]
-    public int Duration { get; set; }
+  [JsonProperty("Duration")] public int Duration { get; set; }
 }
 
 public class Program
 {
-    public static async Task Main()
+  public static async Task Main()
+  {
+    string jsonFileUrl = "https://luegopago.blob.core.windows.net/luegopago-uploads/Pruebas%20LuegoPago/data.json";
+    string jsonContent = await DownloadJsonFromUrl(jsonFileUrl);
+
+    List<Appointment> appointments = JsonConvert.DeserializeObject<List<Appointment>>(jsonContent);
+
+    PrintAllAvailableSpaces(appointments);
+  }
+
+  private static void PrintAllAvailableSpaces(List<Appointment> appointments)
+  {
+    bool switchControl = true;
+    while (switchControl)
     {
-        string jsonFileUrl = "https://luegopago.blob.core.windows.net/luegopago-uploads/Pruebas%20LuegoPago/data.json";
+      Console.WriteLine("Por favor ingrese el día que desea agendar una cita (Ej. Lunes)");
+      string? dayToCheck = NormalizeDayOfTheWeek(Console.ReadLine());
 
-        string jsonContent = await DownloadJsonFromUrl(jsonFileUrl);
+      int? availableSpaces = CalculateAvailableSpaces(appointments, dayToCheck);
 
-        List<Appointment> appointments = JsonConvert.DeserializeObject<List<Appointment>>(jsonContent);
+      if (availableSpaces != null)
+      {
+        Console.WriteLine($"Citas disponibles para el día {dayToCheck} son: {availableSpaces}");
 
-        Console.WriteLine("Por favor ingrese el día que desea agendar una cita (Ej. Lunes)");
-        var dayToCheck = Console.ReadLine();
+        Console.WriteLine("Desea consultar de nuevo? (Si/No)");
+        string? response = Console.ReadLine().ToLower();
 
-        string formatedDayToCheck = Regex.Replace(dayToCheck.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
+        if (response == "no")
+        {
+          switchControl = false;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Por favor ingrese un día válido");
+      }
+    }
+  }
 
-        int availableSpaces = CalculateAvailableSpaces(appointments, formatedDayToCheck);
+  private static async Task<string> DownloadJsonFromUrl(string url)
+  {
+    using HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    if (response.IsSuccessStatusCode)
+    {
+      return await response.Content.ReadAsStringAsync();
+    }
+    else
+    {
+      throw new Exception("Failed to download the JSON file from the provided URL.");
+    }
+  }
 
-        Console.WriteLine($"Citas disponibles para el dia {dayToCheck} son: {availableSpaces}");
+  private static string NormalizeDayOfTheWeek(string dayOfWeek)
+  {
+    return Regex.Replace(dayOfWeek.ToLower().Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
+  }
+
+  private static int? CalculateAvailableSpaces(List<Appointment> appointments, string dayToCheck)
+  {
+    TimeSpan openingTime = TimeSpan.FromHours(9);
+    TimeSpan closingTime = TimeSpan.FromHours(17);
+    TimeSpan minAppointmentDuration = TimeSpan.FromMinutes(30);
+
+    bool anyAppointmentsMatch = appointments.Any(appointment =>
+      NormalizeDayOfTheWeek(appointment.DayOfWeek) == dayToCheck.ToLower());
+
+    if (anyAppointmentsMatch == false)
+    {
+      return null;
     }
 
-    public static async Task<string> DownloadJsonFromUrl(string url)
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadAsStringAsync();
-            }
-            else
-            {
-                throw new Exception("Failed to download the JSON file from the provided URL.");
-            }
-        }
-    }
+    var appointmentsForDay = appointments.Where(appointment =>
+      NormalizeDayOfTheWeek(appointment.DayOfWeek) == dayToCheck.ToLower()).ToList();
 
-    public static int CalculateAvailableSpaces(List<Appointment> appointments, string dayToCheck)
-    {
-        TimeSpan openingTime = TimeSpan.FromHours(9);
-        TimeSpan closingTime = TimeSpan.FromHours(17);
-        TimeSpan minAppointmentDuration = TimeSpan.FromMinutes(30);
+    int totalAppointmentDuration = appointmentsForDay.Sum(appointment => appointment.Duration);
 
-        var appointmentsForDay = appointments.Where(appointment => appointment != null && Regex.Replace(appointment.DayOfWeek.ToLower().Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "") == dayToCheck.ToLower()).ToList();
+    TimeSpan availableTime = closingTime - openingTime;
+    int remainingTime = (int)(availableTime.TotalMinutes - totalAppointmentDuration);
 
-        if (appointmentsForDay == null || appointmentsForDay.Count == 0)
-        {
-            return 0;
-        }
-        else
-        {
-            int totalAppointmentDuration = appointmentsForDay.Sum(appointment => appointment.Duration);
+    int availableSpaces = remainingTime / (int)minAppointmentDuration.TotalMinutes;
 
-            TimeSpan availableTime = closingTime - openingTime;
-            int remainingTime = (int)(availableTime.TotalMinutes - totalAppointmentDuration);
-
-            int availableSpaces = remainingTime / (int)minAppointmentDuration.TotalMinutes;
-
-            return availableSpaces;
-        }
-    }
+    return availableSpaces;
+  }
 }
--- README.md
+++ README.md
@@ -1,9 +1,40 @@
 # Aplicación de Gestión de Citas
 
-Esta aplicación de consola está diseñada para gestionar citas basadas en un archivo JSON alojado en un servidor remoto. Permite verificar la disponibilidad de citas para un día específico y proporciona el número de espacios disponibles para nuevas citas en ese día.
+Esta aplicación de consola está diseñada para gestionar citas basadas en un archivo JSON alojado en un servidor remoto.
+Permite verificar la disponibilidad de citas para un día específico y proporciona el número de espacios disponibles para
+nuevas citas en ese día.
 
 ## Requisitos
+
 Para ejecutar este código, necesitarás tener instalado el paquete NuGet `Newtonsoft.Json`
 
 ## Cómo Ejecutar
-Este proyecto es una aplicación de consola. Puedes ejecutarlo desde tu entorno de desarrollo o usando el comando dotnet run en la terminal después de ubicarte en la carpeta del proyecto.
+
+Este proyecto es una aplicación de consola. Puedes ejecutarlo desde tu entorno de desarrollo o usando el comando dotnet
+run en la terminal después de ubicarte en la carpeta del proyecto.
+
+## Prueba técnica
+
+Un sistema de administración de agendas de servicios cuenta con un módulo que permite la
+programación de citas para dichos servicios.
+Estas citas deben tener una duración mínima de 30
+minutos y una duración máxima de 90 minutos. El horario de atención del sistema se encuentra
+entre las 9:00 y las 17:00 horas.
+Se proporciona un archivo JSON como entrada, el cual contiene un array que hace referencia al día
+de la semana, la hora de inicio y la duración de la cita programada.
+En este contexto, se requiere la creación de un método que tome como parámetro el día de la
+semana que se desea consultar y devuelva el cálculo del total de espacios disponibles para ese día,
+teniendo en cuenta que la duración mínima de una cita es de 30 minutos.
+
+- duración mínima cita = 30 minutos;
+- duración máxima cita = 90 minutos;
+- hora apertura = 9:00 24hr formato; -> TimeSpan
+- hora cierre = 17:00 24hr formato; -> TimeSpan
+- horas laborales = hora apertura - hora cierre = 8 hrs
+- Objeto Cita
+  - dia de la semana -> String
+  - hora de inicio -> TimeSpan
+  - duración cita -> int
+
+
+  
